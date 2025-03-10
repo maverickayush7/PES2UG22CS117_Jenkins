6398807912868e319a52c40b12cc0f0aeed10fbe
Ayush Kumar <ayush@Ayushs-MacBook-Air.local>
2025-03-10 21:33:04 +0530

    task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Changed for task 4 \n";
   return 0;
 }
